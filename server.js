d095f57ccb24b921f3cd89b6311eb4f432f27fff
bcbrian <me@bcbrian.com>
2020-01-03 10:37:02 -0600

    better life

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 var express = require("express");
-var exphbs = require("express-handlebars");
 
 var db = require("./models");
 
@@ -12,15 +11,9 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// Handlebars
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
 // Routes
 const API = require("./routes/apiRoutes");
 API.api(app);
-
-require("./routes/htmlRoutes")(app);
 
 var syncOptions = { force: false };
 
